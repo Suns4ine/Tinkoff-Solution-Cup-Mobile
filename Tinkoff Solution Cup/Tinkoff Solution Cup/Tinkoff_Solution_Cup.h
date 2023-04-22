a656d261121f5befdbf365df9061f023f88f6513
Vyacheslav <pronin.slavok@mail.ru>
2023-04-22 12:05:35 +0300

    Создание проекта

---
+++ Tinkoff Solution Cup/Tinkoff Solution Cup/Tinkoff_Solution_Cup.h
@@ -0,0 +1,18 @@
+//
+//  Tinkoff_Solution_Cup.h
+//  Tinkoff Solution Cup
+//
+//  Created by Vyacheslav Pronin on 22.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tinkoff_Solution_Cup.
+FOUNDATION_EXPORT double Tinkoff_Solution_CupVersionNumber;
+
+//! Project version string for Tinkoff_Solution_Cup.
+FOUNDATION_EXPORT const unsigned char Tinkoff_Solution_CupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tinkoff_Solution_Cup/PublicHeader.h>
+
+
